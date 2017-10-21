a0e2b8f65a762a6250cc39155fbe89111a90b5f8
wzvoid <wzd5211@gmail.com>
2017-10-21 15:43:41 +0800

    init

--- index.cpp
+++ index.cpp
@@ -1,14 +1,15 @@
-//
-// Created by wzvoid on 2017/10/20.
-//
-
-
 #include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 using namespace std;
+using namespace cv;
 
-int main()
-{
-    cout << "success" << endl;
+int main() {
+    Mat in = imread("/Users/wzvoid/CLionProjects/cpp/xm.jpg"), out;
+    namedWindow("xm");
+    imshow("xm", in);
+    waitKey(0);
     return 0;
 }
