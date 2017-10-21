3e7bf2aaa8b7fb025e4a2991cfa65339f7c9d525
wzvoid <wzd5211@gmail.com>
2017-10-21 15:46:07 +0800

    init

--- index.cpp
+++ index.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <fstream>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
