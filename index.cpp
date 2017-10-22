a0161d5019e8cf89d11aa068386c7c7a78726459
wzvoid <wzd5211@gmail.com>
2017-10-22 13:25:08 +0800

    init

--- index.cpp
+++ index.cpp
@@ -6,6 +6,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/ml/ml.hpp>
 
 using namespace std;
 using namespace cv;
