41cb25ccd9b25e7842367922e8e55cfd9da4496d
王振 <wzd5211@gmail.com>
2018-07-16 19:57:52 +0800

    0

--- main.cpp
+++ main.cpp
@@ -5,5 +5,9 @@ using namespace std;
 using namespace cv;
 
 int main() {
-
+    Mat img = imread("/home/wz/CLionProjects/cpp/111.jpg");
+    rectangle(img, cvPoint(72, 79), cvPoint(232, 273), cvScalar(0, 0, 255), 2);
+    namedWindow("test");
+    imshow("test", img);
+    waitKey(0);
 }
