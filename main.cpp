43d73257c58b6ec6bcba62534a1b63bd0a5c198c
wzvoid <wzd5211@gmail.com>
2018-08-31 20:58:33 +0800

    0

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <string>
+#include <memory>
 
 using namespace std;
 
 int main() {
-
+    allocator<string> alloc;  // 定义了一个可以分配string的allocator对象
+    auto const p=alloc.allocate(5);  //分配n个未初始化的string内存，即为n个空string分配了内存，当然正如上面所说，分配的内存是原始的，未构造的。
+    auto q = p;
+    alloc.construct(q++);
+    alloc.construct(q++, 10, 'w');
+    alloc.construct(q++, "we are");
+    q = p;
+    cout << *(++q);
     return 0;
 }
