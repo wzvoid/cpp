83b3d5924609b3dee9768c36defbd083a214fa71
wzvoid <wzd5211@gmail.com>
2018-09-16 12:17:35 +0800

    Merge branch 'master' of https://github.com/wzvoid/cpp
    # Conflicts:
    #	main.cpp

--- tecent/t1.cpp
+++ tecent/t1.cpp
@@ -1,12 +1,8 @@
-//
-// Created by 王振 on 2018/9/15.
-//
-
 #include <iostream>
 #include <string>
+#include <vector>
 #include <deque>
 #include <algorithm>
-#include <vector>
 #include <set>
 #include <unordered_set>
 #include <map>
@@ -14,7 +10,41 @@
 
 using namespace std;
 
+int gcd(int a, int b) {
+    if (a % b == 0) {
+        return b;
+    }
+    return gcd(b, a % b);
+}
+
+int MinMultiple(int a, int b) {
+    return (a * b) / gcd(a, b);
+}
+
+int GB(int a, int b) {
+    if (a == b)
+        return a;
+    if (a < b) {
+        int re = 1;
+        for (int i = a; i <= b; ++i) {
+            re = MinMultiple(re, i);
+        }
+        return re;
+    }
+}
+
 int main() {
-    
+    int n, m;
+    cin >> n;
+    m = n + 1;
+    int re1, re2;
+    re1 = GB(n + 1, m);
+    re2 = GB(1, m);
+    while (re1 != re2) {
+        ++m;
+        re1 = MinMultiple(re1, m);
+        re2 = MinMultiple(re2, m);
+    }
+    cout << m;
     return 0;
 }
