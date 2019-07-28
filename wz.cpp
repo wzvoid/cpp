f8a05d0e175262e0938608da577bc27aa4fda5d0
wzvoid <wzd5211@gmail.com>
2019-07-28 11:18:59 +0800

    0

--- wz.cpp
+++ wz.cpp
@@ -7,14 +7,20 @@
 //#include <unordered_set>
 #include <map>
 #include <cassert>
+#include <forward_list>
 #include <unordered_map>
+#include <array>
 
 using namespace std;
 typedef pair<int, int> pp;
 
 int main() {
-    int a = 1, b = 2;
-    assert(a == b);
-    cout << "success";
+    int *p1 = new int[10];
+    int *p2 = new int;
+    int *p3 = new int(8);
+    for (int i = 1; i <= 10; ++i) {
+        p1[i] = i;
+    }
+    cout << *p3;
     return 0;
 }
