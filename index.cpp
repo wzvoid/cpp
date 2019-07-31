703071fc7540ac9ecbe13334aa7d3b70949d2e56
wzvoid <wzd5211@gmail.com>
2019-07-31 10:34:04 +0800

    wz

--- index.cpp
+++ index.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
-
 using namespace std;
-
 int main() {
-
-    return 0;
+    cout << "sizeof(char):" << sizeof(char) << endl;
+    cout << "sizeof(short):" << sizeof(short) << endl;
+    cout << "sizeof(int):" << sizeof(int) << endl;
+    cout << "sizeof(long):" << sizeof(long) << endl;
+    cout << "sizeof(long long):" << sizeof(long long) << endl;
+    cout << "sizeof(unsigned int):" << sizeof(unsigned int) << endl;
+    cout << "sizeof(float):" << sizeof(float) << endl;
+    cout << "sizeof(double):" << sizeof(double) << endl;
+    void* pointer;
+    cout << "sizeof(pointer):" << sizeof(pointer) << endl;
+return 0;
 }
