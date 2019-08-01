30468a1b4dca9534308828b2dbf9f9612196ebaa
wzvoid <wzd5211@gmail.com>
2019-08-01 09:03:26 +0800

    wz

---
+++ cplusplus/cpointer.cpp
@@ -0,0 +1,40 @@
+//
+// Created by 王振 on 2019-07-31.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <cassert>
+#include <unordered_map>
+
+using namespace std;
+typedef pair<int, int> pp;
+
+int main(int argc, char *argv[]) {
+    int n = 5;
+    int **p = (int **) malloc(sizeof(int *) * n);
+    for (int i = 0; i < n; ++i) {
+        int *q = (int *) malloc(sizeof(int) * n);
+        p[i] = q;
+        for (int j = 0; j < n; ++j) {
+            *(*(p + i) + j) = i + j + 1;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", p[i][j]);
+        }
+        printf("\n");
+    }
+    for (int i = 0; i < n; i++) {
+        free(p[i]);
+    }
+    free(p);
+    return 0;
+}
--- index.cpp
+++ index.cpp
@@ -2,10 +2,15 @@
 #include <string>
 #include <algorithm>
 #include <vector>
+#include <array>
 
 using namespace std;
 
 int main() {
+    array<int, 5> a = {3, 4, 5, 7, 8};
+    for (auto it = a.rbegin(); it != a.rend(); ++it) {
+        cout << *it << ",";
+    }
 
     return 0;
 }
