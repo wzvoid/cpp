7a55f3c99138621dc8cc72ccdb9eff38d0ba2f67
wzvoid <wzd5211@gmail.com>
2019-08-02 13:30:59 +0800

    no message

---
+++ cc/testa.c
@@ -0,0 +1,16 @@
+//
+// Created by 王振 on 2019-08-02.
+//
+
+
+#include <stdio.h>
+#include <math.h>
+#include "testa.h"
+
+double seq2(double x) {
+    return sqrt(x);
+}
+void show(double x) {
+    printf("%f", seq2(x));
+}
+
--- index.cpp
+++ index.cpp
@@ -1,12 +1,13 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <vector>
-#include <array>
+//#include <iostream>
+//#include <string>
+//#include <algorithm>
+//#include <vector>
+//#include <array>
+#include "cc/testa.h"
 
-using namespace std;
+//using namespace std;
 
 int main() {
-
+    show(81);
     return 0;
 }
