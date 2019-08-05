8b99f9809194245fb86043feceac9252803d58a4
wzvoid <wzd5211@gmail.com>
2019-08-05 11:49:20 +0800

    no message

--- cplusplus/clas.cpp
+++ cplusplus/clas.cpp
@@ -13,26 +13,31 @@ public:
 
     ~Base() { cout << "Base 析构" << endl; };
 
-    virtual void show() { cout << "Base show" << endl; }
+    static void show() { cout << "Base show" << endl; }
 
     int a;
 };
 
-class Derived : public Base {
-public:
-    Derived() : Base(55), b(60) { cout << "Derived 构造" << endl; };
-
-    ~Derived() { cout << "Derived 析构" << endl; };
-
-    void show() override { cout << "Derived show" << endl; }
-
-    int b;
-};
+//class Derived : public Base {
+//public:
+//    Derived() : Base(55), b(60) { cout << "Derived 构造" << endl; };
+//
+//    ~Derived() { cout << "Derived 析构" << endl; };
+//
+//    void show() override { cout << "Derived show" << endl; }
+//
+//    int b;
+//};
 
 int main() {
-    Derived d;
+//    Derived d;
 //    Base e;
 //    cout << d.a << endl;
-    d.show();
+//    d.show();
+    enum class WORKDAY {
+        MON, TUE = 9, WED, THU, FRI, SAT, SUN
+    };
+    Base b(50);
+    string s = "ssss";
     return 0;
 }
