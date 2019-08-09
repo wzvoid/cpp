6723dc0e37cbd3302573b9ea9267224cb84a1164
wzvoid <wzd5211@gmail.com>
2019-08-09 09:59:50 +0800

    wz

--- cplusplus/clas.cpp
+++ cplusplus/clas.cpp
@@ -6,33 +6,37 @@ using namespace std;
 class A {
 public:
     int a = 0;
-    int b = 0;
-public:
-    A() : a(0), b(0) {}
+    int c = 0;
 
-    A(int a, int b) : a(a), b(b) {}
+    A() : a(0), c(0) { show(); }
+
+    A(int a, int c) : a(a), c(c) {}
 
     virtual void fun() { cout << "A::fun() called "; }
+
+    void show() {
+        cout << "class A";
+    }
 };
 
 class B : public A {
 public:
     int b;
-public:
+
     B() : b(0) {}
 
     explicit B(int b) : b(b) {}
 
     void fun() override { cout << "B::fun() called "; }
-};
 
-//class C : public B {
-//public:
-//    void fun() override { cout << "C::fun() called "; }
-//};
+    void show() {
+        cout << "class B:" << b;
+    };
+};
 
 int main() {
     A a;
-    a.fun();
+    B b;
+//    a.fun();
     return 0;
 }
