d2d62d7903a282d0f7801a12b06e58b6e9794f08
wzvoid <wzd5211@gmail.com>
2019-08-13 22:47:48 +0800

    wz

--- wz.cpp
+++ wz.cpp
@@ -1,7 +1,7 @@
 #include "my_stl/wz_string.h"
 
 int main() {
-    wz_string a = "wz", b = "void", c;
+    wz_string a = "wz", b="void", c;
     c = a + b;
 //    std::cin >> w;
     std::cout << a.length() << std::endl;
