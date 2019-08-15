d82643343244cd11bb48aa18b0c88c92b6b7ce11
wzvoid <wzd5211@gmail.com>
2019-08-16 00:00:18 +0800

    wz

--- index.cpp
+++ index.cpp
@@ -9,21 +9,21 @@
 
 using namespace std;
 
+template<typename T>
+T compare(T x, T y) {
+    return x > y ? x : y;
+}
+
+
 int main() {
-    string s1 = "wzvoid";
-    const char *s2 = "wzvoid";
-    string *s3 = new string("wzvoid");
-    char *s4 = (char *) malloc(sizeof(char) * 6);
-    cout << sizeof(s1) << endl;
-    cout << sizeof(s2) << endl;
-    cout << sizeof(s3) << endl;
-    cout << sizeof(s4) << endl;
-//    map<string, int> m;
-//    m["w"] = 21;
-//    m["c"] = 20;
-//    m["a"] = 19;
-//    if (m.find("b") == m.end())
-//        cout << "no b";
-//    else cout << m["a"];
+//    string s1 = "wzvoid";
+//    const char *s2 = "wzvoid";
+//    string *s3 = new string("wzvoid");
+//    char *s4 = (char *) malloc(sizeof(char) * 6);
+//    cout << sizeof(s1) << endl;
+//    cout << sizeof(s2) << endl;
+//    cout << sizeof(s3) << endl;
+//    cout << sizeof(s4) << endl;
+    cout << compare(4, 6);
     return 0;
 }
