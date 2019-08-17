da342a60254b2a27f5c29530e58a7e3a19c882d9
wzvoid <wzd5211@gmail.com>
2019-08-17 11:22:45 +0800

    no message

--- cpp/vector.cpp
+++ cpp/vector.cpp
@@ -2,3 +2,22 @@
 // Created by wzvoid on 2018/8/31.
 //
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    std::vector<int> myvector{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::vector<int>::iterator it;
+
+//    it = myvector.begin();
+//    it = myvector.insert(it, 200);
+    auto it2 = myvector.erase(myvector.begin() + 1);
+    for (auto &c:myvector)
+        cout << c << ",";
+    cout << endl << *(it2);
+    return 0;
+}
