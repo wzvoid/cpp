bebb28e0362d56458f7214ba49ccb0802ab407fb
wzvoid <wzd5211@gmail.com>
2019-08-17 11:32:49 +0800

    no message

--- cpp/vector.cpp
+++ cpp/vector.cpp
@@ -10,14 +10,18 @@
 using namespace std;
 
 int main() {
-    std::vector<int> myvector{1, 2, 3, 4, 5, 6, 7, 8, 9};
-    std::vector<int>::iterator it;
+//    std::vector<int> myvector{1, 2, 3, 4, 5, 6, 7, 8, 9};
+//    std::vector<int>::iterator it;
+//
+////    it = myvector.begin();
+////    it = myvector.insert(it, 200);
+//    auto it2 = myvector.erase(myvector.begin() + 1);
+//    for (auto &c:myvector)
+//        cout << c << ",";
+//    cout << endl << *(it2);
 
-//    it = myvector.begin();
-//    it = myvector.insert(it, 200);
-    auto it2 = myvector.erase(myvector.begin() + 1);
-    for (auto &c:myvector)
-        cout << c << ",";
-    cout << endl << *(it2);
+    string s = "we are happy";
+    s.insert(2, "%20");
+    cout << s;
     return 0;
 }
