99be77c3448096da9084b969e80b43794a1f2fa2
王振 <wzd5211@gmail.com>
2019-08-18 16:13:02 +0800

    wz

--- wz.cpp
+++ wz.cpp
@@ -1,11 +1,15 @@
 #include "my_stl/wz_string.h"
-
+#include <iostream>
 int main() {
-    wz_string p = "void ", w = "wz";
-    w = p;
-    std::cin >> w;
-    std::cout << w.length() << std::endl;
-    std::cout << w.empty() << std::endl;
-    std::cout << w + p << std::endl;
+//    wz_string p = "void ", w = "wz";
+//    w = p;
+//    std::cin >> w;
+//    std::cout << w.length() << std::endl;
+//    std::cout << w.empty() << std::endl;
+//    std::cout << w + p << std::endl;
+    int sum=0,value=0;
+    while(std::cin >> value)
+        sum += value;
+    std::cout << sum;
     return 0;
 }
