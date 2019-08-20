7b14fa7a569c48e8f81bff00aabf60f3912b8ab7
wzvoid <wzd5211@gmail.com>
2019-08-20 18:02:04 +0800

    no message

--- cpp/vector.cpp
+++ cpp/vector.cpp
@@ -26,8 +26,25 @@ int main() {
 //        cout << c << ",";
 //    cout << endl << *(it2);
 
-    string *s = new string("wearehappy");
-    s->resize(3);
-    cout << *s;
+//    string *s = new string("wearehappy");
+//    s->resize(3);
+//    cout << *s;
+    vector<int> v{15, 1, 3, 5, 7, 4, 2, 4, 8, 29};
+    int left = v[0], right = 0;
+    for (vector<int>::size_type i = 2; i < v.size(); ++i) {
+        right += v[i];
+    }
+    cout << left << "," << right << endl;
+    vector<int>::size_type j = 1;
+    for (; j < v.size() - 1; ++j) {
+        if (left == right) break;
+        left += v[j];
+        right -= v[j + 1];
+        cout << left << "," << right << endl;
+    }
+    if (right == 0)
+        cout << "none";
+    else
+        cout << v[j];
     return 0;
 }
