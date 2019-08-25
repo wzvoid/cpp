6cf85333f8da853c9b957064347aee6e393dca90
王振 <wzd5211@gmail.com>
2019-08-25 20:32:13 +0800

    wz

--- index.cpp
+++ index.cpp
@@ -1,22 +1,74 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
 #include <vector>
-#include <array>
-#include <list>
-#include <map>
-#include <ext/hash_map>
 
 using namespace std;
 
 int main() {
-//    string s1 = "wzvoid";
-//    const char *s2 = "wzvoid";
-//    string *s3 = new string("wzvoid");
-//    char *s4 = (char *) malloc(sizeof(char) * 6);
-//    cout << sizeof(s1) << endl;
-//    cout << sizeof(s2) << endl;
-//    cout << sizeof(s3) << endl;
-//    cout << sizeof(s4) << endl;
+    int d;
+    cin >> d;
+    vector<vector<int>> v;
+    for (int i = 0; i < 4; i++) {
+        vector<int> t;
+        for (int j = 0; j < 4; j++) {
+            int val;
+            cin >> val;
+            t.push_back(val);
+        }
+        v.push_back(t);
+    }
+
+    cout << endl;
+    for (auto &c:v) {
+        for (auto b:c)
+            cout << b << " ";
+        cout << endl;
+    }
+    cout << endl;
+
+    if (d == 2) {
+        for (int i = 0; i <= 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                if ((v[j][i] == v[j + 1][i]) || (v[j + 1][i] == 0)) {
+                    v[j + 1][i] += v[j][i];
+                    v[j][i] = 0;
+                }
+            }
+        }
+    } else if (d == 1) {
+        for (int i = 0; i <= 3; ++i) {
+            for (int j = 2; j >= 0; --j) {
+                if (v[j][i] == v[j + 1][i]) {
+                    v[j][i] += v[j + 1][i];
+                    v[j + 1][i] = 0;
+                }
+
+            }
+        }
+    } else if (d == 4) {
+        for (int i = 0; i <= 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                if ((v[i][j] == v[i][j + 1]) || (v[i][j + 1] == 0)) {
+                    v[i][j + 1] += v[i][j];
+                    v[i][j] = 0;
+                }
+            }
+        }
+    } else if (d == 3) {
+        for (int i = 0; i <= 3; ++i) {
+            for (int j = 3; j > 0; --j) {
+                if ((v[i][j] == v[i][j - 1]) || (v[i][j - 1] == 0)) {
+                    v[i][j - 1] += v[i][j];
+                    v[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    for (auto &c:v) {
+        for (auto b:c)
+            cout << b << " ";
+        cout << endl;
+    }
+
     return 0;
 }
