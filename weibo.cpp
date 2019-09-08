b3db0f3b5dca25b0753f6a8b62999338264a5649
wzvoid <wzd5211@gmail.com>
2019-09-08 17:03:55 +0800

    wz

--- weibo.cpp
+++ weibo.cpp
@@ -1,9 +1,59 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include <cmath>
+#include <algorithm>
 
 using namespace std;
 
-int main() {
+void permutation(vector<vector<int>> &arry, vector<int> &nums, int start, int end) {
+    if (start == end) {
+        vector<int> temp;
+        for (int i = 0; i <= end; ++i) {
+            temp.push_back(nums[i]);
+        }
+        arry.push_back(temp);
+    } else {
+        for (int i = start; i <= end; ++i) {
+            swap(nums[start], nums[i]);
+            permutation(arry, nums, start + 1, end);
+            swap(nums[start], nums[i]);
+        }
+    }
+}
 
+vector<vector<int>> permute(vector<int> &nums) {
+    vector<vector<int>> permut;
+    permutation(permut, nums, 0, nums.size() - 1);
+    return permut;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> A, P;
+    for (int i = 0; i < n - 1; ++i) {
+        int temp;
+        cin >> temp;
+        A.push_back(temp);
+    }
+    for (int j = 0; j < n; ++j) {
+        P.push_back(j + 1);
+    }
+    vector<vector<int>> pp = permute(P);
+    int count = 0;
+    for (int k = 0; k < pp.size(); ++k) {
+        vector<int> temp = pp[k];
+        int flag = 1;
+        for (int i = 0; i < A.size(); ++i) {
+            if ((A[i] == 0 && temp[i] < temp[i + 1]) || (A[i] == 1 && temp[i] > temp[i + 1]))
+                continue;
+            else {
+                flag = 0;
+                break;
+            }
+        }
+        if (flag == 1)
+            ++count;
+    }
+    cout << (count % (int) (pow(10, 9) + 7));
 }
--- wz.cpp
+++ wz.cpp
@@ -1,12 +1,31 @@
-#include "my_stl/wz_string.h"
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+double win_rate(int red, int blue) {
+    if (red <= 0)
+        return 0.0;
+    if (blue < 0)
+        return 1;
+    if (red == 1 && blue == 1)
+        return 0.5;
+    if (red == 1 && blue == 0)
+        return 1;
+    double res = ((double) red / (red + blue));
+    res += ((double) blue / (red + blue)) * ((double) (blue - 1) / (red + blue - 1)) *
+           ((double) (blue - 2) / (red + blue - 2)) * win_rate(red, blue - 3) +
+           (double) red / (red + blue - 2) * win_rate(red - 1, blue - 2);
+    return res;
+}
 
 int main() {
-    wz_string a = "wz", b="void", c;
-    c = a + b;
-//    std::cin >> w;
-    std::cout << a.length() << std::endl;
-    std::cout << a.empty() << std::endl;
-    std::cout << c << std::endl;
-//    std::cout << (w + p).length() << std::endl;
+    int n, m;
+    cin >> n >> m;
+    cout << setiosflags(ios::fixed) << setprecision(5) << win_rate(n, m);
     return 0;
 }
