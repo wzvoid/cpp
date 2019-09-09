ae1ac2df0b1802a954876a1192bfb89964ccc967
wzvoid <wzd5211@gmail.com>
2019-09-09 23:12:55 +0800

    wz

--- wz.cpp
+++ wz.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <vector>
 #include <cmath>
@@ -7,25 +6,7 @@
 
 using namespace std;
 
-double win_rate(int red, int blue) {
-    if (red <= 0)
-        return 0.0;
-    if (blue < 0)
-        return 1;
-    if (red == 1 && blue == 1)
-        return 0.5;
-    if (red == 1 && blue == 0)
-        return 1;
-    double res = ((double) red / (red + blue));
-    res += ((double) blue / (red + blue)) * ((double) (blue - 1) / (red + blue - 1)) *
-           ((double) (blue - 2) / (red + blue - 2)) * win_rate(red, blue - 3) +
-           (double) red / (red + blue - 2) * win_rate(red - 1, blue - 2);
-    return res;
-}
-
 int main() {
-    int n, m;
-    cin >> n >> m;
-    cout << setiosflags(ios::fixed) << setprecision(5) << win_rate(n, m);
+
     return 0;
 }
