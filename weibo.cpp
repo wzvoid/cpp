cc6042b0621c852ab832c55a40cc8f98c6d5b052
wzvoid <wzd5211@gmail.com>
2019-09-18 09:18:14 +0800

    wz

--- weibo.cpp
+++ weibo.cpp
@@ -2,10 +2,12 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
 int main() {
-
+    size_t res;
     return 0;
 }
