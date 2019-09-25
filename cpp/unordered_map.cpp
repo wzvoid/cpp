fdf442318c104bd5fdee468310c1d6402b72cd73
wzvoid <wzd5211@gmail.com>
2019-09-25 21:42:12 +0800

    00

--- cpp/unordered_map.cpp
+++ cpp/unordered_map.cpp
@@ -1,8 +1,3 @@
-//
-// Created by wzvoid on 2018/8/30.
-//
-
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,41 +5,38 @@
 #include <unordered_map>
 #include <set>
 #include <unordered_set>
+#include <utility>
 
 using namespace std;
 
-bool duplicate(int numbers[], int length, int *duplication) {
-    if (length <= 0)
-        return false;
-    unordered_map<int, int> um;
-    for (int i = 0; i < length; ++i) {
-        if (um.find(numbers[i]) != um.end()) {
-            um[numbers[i]] += 1;
-            *duplication = numbers[i];
-            return true;
-        } else {
-            um[numbers[i]] = 1;
-        }
-    }
-    return false;
-}
-
 int main() {
-    unordered_map<string, int> mymap = {
+    map<string, int> mymap = {
             {"Mars",    3000},
-            {"Saturn",  6000},
-            {"Jupiter", 7000},
-            {"Earth",   5000}};
+            {"Saturn",  4000},
+            {"Jupiter", 5000},
+            {"Earth",   6000}};
 
     mymap.at("Mars") = 3396;
     mymap["Mars"] = 4000;
+    mymap["Mars"] = 4001;
+    mymap.insert(pair<string, int>("Mars", 4001));
+    mymap.insert(make_pair("Mars", 4003));
+    mymap.insert(make_pair<string, int>("Mars", 4003));
     for (auto &x: mymap) {
         std::cout << x.first << ": " << x.second << std::endl;
     }
+    if (mymap.erase("Saturn"))
+        cout << "Saturn!" << endl;
+    else
+        cout << "Not Found!" << endl;
     for (auto &x: mymap) {
-        std::cout << "Element [" << x.first << ":" << x.second << "]";
-        std::cout << " is in bucket #" << mymap.bucket(x.first) << std::endl;
+        std::cout << x.first << ": " << x.second << std::endl;
     }
+//    for (auto &x: mymap) {
+//        std::cout << "Element [" << x.first << ":" << x.second << "]";
+//        std::cout << " is in bucket #" << mymap.bucket(x.first) << std::endl;
+//    }
+
 //    unsigned nbuckets = mymap.bucket_count();
 //    std::cout << "mymap has " << nbuckets << " buckets:\n";
 //    for (unsigned i = 0; i < nbuckets; ++i) {
@@ -52,29 +44,22 @@ int main() {
 //    }
 //    cout << mymap.max_load_factor();
 
-    unordered_map<char, int> count;
-    unordered_map<char, int>::iterator it;
-    string s = "we are family";
-    for (char &c:s) {
-        if (count.find(c) != count.end()) {
-            ++count[c];
-        } else {
-            count[c] = 1;
-        }
-    }
-    for (auto &tmp:count) {
-        cout << tmp.first << " " << tmp.second << endl;
-    }
-
-    count.insert(make_pair<char, int>('U', 2));
-    count.insert({'W', 4});
-    cout << (typeid(it) == typeid(count));
-
-    int a[] = {5, 4, 3, 2, 7, 6, 5, 5};
-    int b[10];
-    duplicate(a, 8, b);
-    cout << b[0];
-
-
+//    unordered_map<char, int> count;
+//    unordered_map<char, int>::iterator it;
+//    string s = "we are family";
+//    for (char &c:s) {
+//        if (count.find(c) != count.end()) {
+//            ++count[c];
+//        } else {
+//            count[c] = 1;
+//        }
+//    }
+//    for (auto &tmp:count) {
+//        cout << tmp.first << ": " << tmp.second << endl;
+//    }
+//
+//    count.insert(make_pair<char, int>('U', 2));
+//    count.insert({'W', 4});
+//    cout << (typeid(it) == typeid(count));
     return 0;
 }
--- t5.cpp
+++ t5.cpp
@@ -1,42 +1,72 @@
-#include<iostream>
-#include <vector>
-#include <unordered_set>
+#include <iostream>
 #include <unordered_map>
+#include <list>
+#include <utility>
 
 using namespace std;
+using namespace stdext;
 
-int main() {
-    int n, m;
-    cin >> n;
-    vector<int> v;
-    for (int i = 0; i < n; ++i) {
-        int temp;
-        cin >> temp;
-        v.push_back(temp);
+class LRUCache {
+private:
+    int m_capacity;                                                        //cashe的大小
+    list<pair<int, int> > caches;                                          //用一个双链表存储cashe的内容
+    unordered_map<int, list<pair<int, int>>::iterator> cachesMap;         //使用map加快查找的速度
+public:
+    explicit LRUCache(int capacity) { m_capacity = capacity; }
+
+    int get(int key) {
+        int retValue = -1;
+        unordered_map<int, list<pair<int, int>>::iterator>::iterator it = cachesMap.find(key);
+        //如果在Cashe中，将记录移动到链表的最前端
+        if (it != cachesMap.end()) {
+            retValue = it->second->second;
+            //移动到最前端
+            list<pair<int, int>>::iterator ptrPair = it->second;
+            pair<int, int> tmpPair = *ptrPair;
+            caches.erase(ptrPair++);
+            caches.push_front(tmpPair);
+            //修改map中的值
+            cachesMap[key] = caches.begin();
+        }
+        return retValue;
     }
-    cin >> m;
-    vector<pair<int, int>> q;
-    for (int j = 0; j < m; ++j) {
-        int l, r;
-        cin >> l >> r;
-        q.emplace_back(make_pair(l, r));
-    }
-    for (int k = 0; k < m; ++k) {
-        int l = q[k].first, r = q[k].second;
-        unordered_map<int, int> s;
-        int res = 0;
-        for (int i = l - 1; i <= r - 1; ++i) {
-            if (s.find(v[i]) == s.end()) {
-                s[v[i]] = 1;
-            } else {
-                s[v[i]] += 1;
+
+    void set(int key, int value) {
+        unordered_map<int, list<pair<int, int>>::iterator>::iterator it = cachesMap.find(key);
+        if (it != cachesMap.end()) { //已经存在其中
+            list<pair<int, int>>::iterator ptrPait = it->second;
+            ptrPait->second = value;
+            //移动到最前面
+            pair<int, int> tmpPair = *ptrPait;
+            caches.erase(ptrPait);
+            caches.push_front(tmpPair);
+            //更新map
+            cachesMap[key] = caches.begin();
+        } else {//不存在其中
+            pair<int, int> tmpPair = make_pair(key, value);
+            if (m_capacity == caches.size()) //已经满
+            {
+                int delKey = caches.back().first;
+                caches.pop_back(); //删除最后一个
+                //删除在map中的相应项
+                unordered_map<int, list<pair<int, int>>::iterator>::iterator delIt = cachesMap.find(delKey);
+                cachesMap.erase(delIt++);
             }
+            caches.push_front(tmpPair);
+            cachesMap[key] = caches.begin(); //更新map
         }
-        for (auto it = s.begin(); it != s.end(); ++it) {
-            if (it->second == 1)
-                ++res;
-        }
-        cout << res << endl;
     }
+};
+
+int main(int argc, char **argv) {
+    LRUCache s(2);
+    s.set(2, 1);
+    s.set(1, 1);
+    cout << s.get(2) << endl;
+    s.set(4, 1);
+    s.set(5, 2);
+    cout << s.get(5) << endl;
+    cout << s.get(4) << endl;
+    getchar();
     return 0;
 }
